c5431b012abea0e422addba6c9e739b7c2bc72d4
WWWw <109729945+initialencounter@users.noreply.github.com>
2025-01-28 23:48:21 +0800

    feat(anti-nsfw): server & client

--- packages/anti-nsfw/src/index.ts
+++ packages/anti-nsfw/src/index.ts
@@ -2,29 +2,73 @@ import { readFileSync } from 'fs';
 import { Context, Schema, h } from 'koishi'
 import { } from 'koishi-plugin-transformers';
 import { } from '@initencounter/jimp'
+import { } from '@koishijs/plugin-server'
+import { } from '@koishijs/plugin-http'
+import type { RawImage } from '@huggingface/transformers';
 import { resolve } from 'path';
 export const name = 'anti-nsfw'
 export const inject = {
-  required: ['transformers', 'jimp']
+  required: ['transformers', 'jimp'],
+  optional: ['server', 'http']
 }
 
 export const usage = readFileSync(resolve(__dirname, "../readme.md")).toString('utf-8')
 export interface Config {
-  cacheDir: string
-  model: string
-  score: number
-  nsfw_channel: string[]
+  implServerPath?: string
+  cacheDir?: string
+  model?: string
+  score?: number
+  nsfw_channel?: string[]
+  runAs: 'client' | 'server' | 'local'
+  endpoint?: string
 }
-export const Config: Schema<Config> = Schema.object({
-  cacheDir: Schema.string().default('').description('缓存目录'),
-  model: Schema.string().default('AdamCodd/vit-base-nsfw-detector').description('模型名称'),
-  score: Schema.number().role('slider').min(0).max(1).step(0.01).default(0.8).description('允许的 nsfw 值，调为 1 则不再撤回，调为 0 则 100% 撤回'),
-  nsfw_channel: Schema.array(Schema.string()).default([]).description('允许发送 nsfw 图片的频道')
-})
+export const Config: Schema<Config> = Schema.intersect([
+  Schema.object({
+    runAs: Schema.union(['client', 'server', 'local']).default('local').description('运行模式'),
+  }),
+  Schema.union([
+    Schema.object({
+      runAs: Schema.const('client'),
+      endpoint: Schema.string().default('http://127.0.0.1:5141/nsfw-detect').description('服务端地址'),
+      score: Schema.number().role('slider').min(0).max(1).step(0.01).default(0.8).description('允许的 nsfw 值，调为 1 则不再撤回，调为 0 则 100% 撤回'),
+      nsfw_channel: Schema.array(Schema.string()).default([]).description('允许发送 nsfw 图片的频道'),
+    }),
+    Schema.object({
+      runAs: Schema.const('server'),
+      cacheDir: Schema.string().required().description('缓存目录'),
+      model: Schema.string().default('AdamCodd/vit-base-nsfw-detector').description('模型名称'),
+      implServerPath: Schema.string().default('/nsfw-detect').description('服务端路径'),
+    }),
+    Schema.object({
+      runAs: Schema.const('local'),
+      cacheDir: Schema.string().required().description('缓存目录'),
+      model: Schema.string().default('AdamCodd/vit-base-nsfw-detector').description('模型名称'),
+      score: Schema.number().role('slider').min(0).max(1).step(0.01).default(0.8).description('允许的 nsfw 值，调为 1 则不再撤回，调为 0 则 100% 撤回'),
+      nsfw_channel: Schema.array(Schema.string()).default([]).description('允许发送 nsfw 图片的频道'),
+    }),
+  ]),
+
+])
 
 export function apply(ctx: Context, config: Config) {
   let classifier = null
+  if (ctx.server && (config.runAs === 'server')) {
+    ctx.server.post(config.implServerPath, async (ctx2) => {
+      if (!classifier) {
+        ctx2.status = 500
+        ctx2.body = 'The classifier is not ready yet. Please try again later.'
+      }
+      let img = ctx2.request.body['img']
+      img = await imagePrepare(img, ctx)
+      let res = await classifier(img)
+      ctx2.status = 200
+      ctx2.body = res
+    })
+  };
   ctx.on('ready', async () => {
+    if (config.runAs === 'client') {
+      return
+    }
     classifier = await ctx.transformers.getPipeline('image-classification', config.model, config.cacheDir)
     if (!classifier) {
       ctx.logger(name).warn(`模型加载失败, 名称：${config.model}, 缓存目录：${config.cacheDir}`)
@@ -34,12 +78,15 @@ export function apply(ctx: Context, config: Config) {
   })
 
   ctx.middleware(async (session, next) => {
+    if (config.runAs === 'server') {
+      return next()
+    }
     if (config.nsfw_channel.includes(session.channelId)) {
       return next()
     }
     for (let message of session.elements) {
       if (message.type === "img") {
-        if (!classifier) {
+        if (!classifier && config.runAs === 'local') {
           ctx.logger(name).warn('The classifier is not ready yet. Please try again later.')
         }
         let start = Date.now()
@@ -47,20 +94,18 @@ export function apply(ctx: Context, config: Config) {
         if (img.startsWith('file://')) {
           img = img.replace('file://', '')
         }
-        if (img.startsWith('data:image')) {
-          let base64 = img.split(',')[1]
-          const imgBuffer = Buffer.from(base64, 'base64')
-          // const base64Header = img.split(',')[0]
-          // const channels = getChannelCount(imgBuffer, base64Header)
-          const jimp = await ctx.jimp.read(imgBuffer)
-          const imgUint8Array = new Uint8Array(jimp.bitmap.data)
-          const width = jimp.bitmap.width
-          const height = jimp.bitmap.height
-          // bug only support 4 channels
-          img = await ctx.transformers.newRawImage(imgUint8Array, width, height, 4)
-        }
         // @ts-ignore
-        let res = await classifier(img)
+        let res: any;
+        if (config.runAs === 'client') {
+          await ctx.http.post(config.endpoint, { img }, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+        } else {
+          img = await imagePrepare(img, ctx)
+          res = await classifier(img)
+        }
         let end = Date.now()
         ctx.logger(name).info(`Time taken: ${end - start}ms`)
         ctx.logger(name).info(`【${session.channelId} | ${session.userId}】：[${res[0].label} | ${res[0].score}]`)
@@ -74,6 +119,22 @@ export function apply(ctx: Context, config: Config) {
   })
 }
 
+async function imagePrepare(img: string, ctx: Context): Promise<string | RawImage> {
+  let res: string | RawImage
+  if (img.startsWith('data:image')) {
+    let base64 = img.split(',')[1]
+    const imgBuffer = Buffer.from(base64, 'base64')
+    // const base64Header = img.split(',')[0]
+    // const channels = getChannelCount(imgBuffer, base64Header)
+    const jimp = await ctx.jimp.read(imgBuffer)
+    const imgUint8Array = new Uint8Array(jimp.bitmap.data)
+    const width = jimp.bitmap.width
+    const height = jimp.bitmap.height
+    // bug only support 4 channels
+    res = await ctx.transformers.newRawImage(imgUint8Array, width, height, 4)
+  }
+  return res
+}
 export function getChannelCount(buffer: Buffer, base64Header: string): number {
   if (base64Header.includes('png')) {
     return getPngChannelCount(buffer)
